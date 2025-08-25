36f46d93b57b665216679823604426bd9173dbc3
TLLRSK <tullarisk@gmail.com>
2025-08-25 16:26:51 +0200

    Refactor: Moving gsap introAnimation to utils

--- app/components/navbar/Navbar.vue
+++ app/components/navbar/Navbar.vue
@@ -1,48 +1,11 @@
 <script setup lang="ts">
-import gsap from "gsap";
 import IconRoy from "../icons/IconRoy.vue";
+import { introAnimation } from "~/utils/animations/gsap";
 
 onMounted(() => {
   introAnimation();
 });
 
-const introAnimation = () => {
-  gsap.to(".nav", { backgroundColor: "#102D2D" });
-
-  const tl = gsap.timeline({ delay: 1.6 });
-
-  tl.to(
-    ".nav",
-    {
-      duration: 1.5,
-      height: "6.2rem",
-      backgroundColor: "#FFFFFF",
-      ease: "power2.inOut",
-      borderColor: "#F0F0F0",
-    },
-    "start"
-  );
-
-  tl.to(
-    ".logo",
-    {
-      duration: 1,
-      scale: 1,
-      ease: "power2.inOut",
-    },
-    "-=2.8"
-  );
-
-  tl.to(
-    ".logo",
-    {
-      duration: 0.5,
-      color: "#102D2D",
-      ease: "power2.inOut",
-    },
-    "-=1"
-  );
-};
 </script>
 
 <template>
---
+++ app/utils/animations/gsap.ts
@@ -0,0 +1,39 @@
+import gsap from "gsap";
+
+export const introAnimation = () => {
+  gsap.to(".nav", { backgroundColor: "#102D2D" });
+
+  const tl = gsap.timeline({ delay: 1.6 });
+
+  tl.to(
+    ".nav",
+    {
+      duration: 1.5,
+      height: "6.2rem",
+      backgroundColor: "#FFFFFF",
+      ease: "power2.inOut",
+      borderColor: "#F0F0F0",
+    },
+    "start"
+  );
+
+  tl.to(
+    ".logo",
+    {
+      duration: 1,
+      scale: 1,
+      ease: "power2.inOut",
+    },
+    "-=2.8"
+  );
+
+  tl.to(
+    ".logo",
+    {
+      duration: 0.5,
+      color: "#102D2D",
+      ease: "power2.inOut",
+    },
+    "-=1"
+  );
+};
