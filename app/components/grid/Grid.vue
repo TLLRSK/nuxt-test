0429bba9910eebaca400991feeb6820e1327c35d
TLLRSK <tullarisk@gmail.com>
2025-08-26 10:24:54 +0200

    Fixed: Loadmore button hydration mismatch

--- app/components/grid/Grid.vue
+++ app/components/grid/Grid.vue
@@ -29,10 +29,15 @@ const { yachts, pending, error, hasNextPage, loadMore } = useYachts();
       </div>
     </ClientOnly>
 
-    <div v-if="!pending && hasNextPage && yachts.length > 0" class="load-more-container">
-      <button @click="loadMore" class="btn--secondary text-sm">
-        Load More
-      </button>
-    </div>
+    <ClientOnly>
+      <div
+        v-if="!pending && hasNextPage && yachts.length > 0"
+        class="load-more-container"
+      >
+        <button @click="loadMore" class="btn--secondary text-sm">
+          Load More
+        </button>
+      </div>
+    </ClientOnly>
   </section>
 </template>
